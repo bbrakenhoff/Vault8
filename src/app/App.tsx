1cf9bf33a8f1aec2be4f4cb2980b3e95f62016ae
Bijoya Brakenhoff <bbrakenhoff@users.noreply.github.com>
2024-12-10 21:02:57 +0100

    Add routing to project

---
+++ src/app/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+
+    return (
+        <>
+            <h1>Hier komt de app</h1>
+        </>
+    )
+}
+
+export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import {StrictMode} from 'react'
+import {createRoot} from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import {createBrowserRouter, RouterProvider} from "react-router";
+import App from "./app/App.tsx";
+import Dashboard from "./app/components/Dashboard.tsx";
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <App />,
+        children: [{
+            path: 'dashboard',
+            element: <Dashboard />
+        }]
+    }
+])
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <StrictMode>
+        <RouterProvider router={router}/>
+    </StrictMode>,
 )
